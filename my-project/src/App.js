3497a7480d4c21ba7fd0cefa9c3a9dcdeeeb3523
Xiaole Su <suxiaole0223@gmail.com>
2024-09-14 20:51:03 -0400

    hiya

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,27 +1,30 @@
 import './App.css';
 import Player from "./components/Player.js"
-import Upload from './components/Upload.js';
+import Moves from './components/Moves.js';
 
 function App() {
   return (
-    <div>
-      <div class="grid grid-cols-2 gap-4 w-full min-h-screen">
-       
-
-        <div class="space-y-4">
-          <div class="bg-green-100 p-4">
+    <div class="text-white">
+      <div class="bg-[#CE8397]">Title</div>
+      <div class="grid grid-cols-3 w-full min-h-screen">
+        <div class="col-span-2 bg-[#121A2F]">
+          <div class=" p-4">
             <p>Song Name genre</p>
           </div>
-          <div class="bg-green-100 p-4">
+          <div class="p-4">
             <p>Moving slides</p>
           </div>
-          <div class="bg-green-100 p-4">
+          <div class="p-4">
             <Player />
           </div>
-        </div>
-        <div class="bg-blue-100 p-4">
+      </div>
+        
+      <div class="col-span-1 bg-[#1A2335] p-4">
           <p>Moves</p>
-          
+          <div class="overflow-scroll
+                w-full mx-200 h-screen text-justify">
+          <Moves />
+          </div>
         </div>
       </div>
 
--- my-project/src/components/Moves.js
+++ my-project/src/components/Moves.js
@@ -1,10 +1,26 @@
 import '../App.css';
-
+import { useState } from 'react';
+import { AMove } from './AMove.js';
 
 function Moves() {
+
+  const allmoves = [1,2,3,4,5,6,7,8,9,10,11,12];
+
+  const searchBar = () => {}
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+  };
+  
   return (
-    <div>
-      
+    <div className="grid grid-rows-10 gap-x-4">
+      <div class="row-span-9 flex flex-wrap">
+        {allmoves.map((n) => {
+          return <AMove number={n}/>
+        })}
+      </div>
     </div>
 
   );
